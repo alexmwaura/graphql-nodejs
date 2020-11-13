8ae702399dabf796e6f84e80b85643faad16830f
alexmwaura <alexmwaura43@gmail.com>
2020-11-13 13:30:00 +0300

    update graphqlHttp function

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 "use strict";
 
 const express = require("express");
-const graphqlHTTp = require("express-graphql");
+const {graphqlHTTP} = require("express-graphql");
 
 const app = express();
+app.use("/graphql",graphqlHTTP({
+    graphiql: true
+}))
 
 app.listen(4000, () => {
   console.log("now listening for requests on port 4000");
